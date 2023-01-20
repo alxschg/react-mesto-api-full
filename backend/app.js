fbafd32ccab31d6014783eda3ef82c95c8c91540
Alexei Scheglov <aleshascheglov@yandex.ru>
2023-01-20 11:08:33 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -26,30 +26,23 @@ mongoose.connect('mongodb://localhost:27017/mestodb')
     console.error(err);
   });
 
-// eslint-disable-next-line consistent-return
 app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
+  const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
-
-  next();
+  return next();
 });
 
 app.use(requestLogger);
