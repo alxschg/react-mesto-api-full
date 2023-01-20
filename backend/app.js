c14a6052a4b980181e4b3bd34d73b6b4adac6f94
Alexei Scheglov <aleshascheglov@yandex.ru>
2023-01-20 10:58:04 +0300

    try

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const cors = require('cors');
+
+const allowedCors = [
+  'https://mesto.alxschg.nomoredomains.rocks',
+  'http://mesto.alxschg.nomoredomains.rocks',
+  'localhost:3000',
+];
 
 const { routes } = require('./routes');
 const { handleError } = require('./middlewares/handleError');
@@ -21,7 +26,32 @@ mongoose.connect('mongodb://localhost:27017/mestodb')
     console.error(err);
   });
 
-app.use(cors());
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+
+  next();
+});
+
 app.use(requestLogger);
 
 app.get('/crash-test', () => {
