788ccb4c0aa9e0ab6c64c7a83c86a46e90d5d187
Alexei Scheglov <aleshascheglov@yandex.ru>
2023-01-25 04:05:28 +0300

    fix: убрал неиспользованную переменную

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cors = require('cors');
-const path = require('path');
 
 const { routes } = require('./routes');
 const { handleError } = require('./middlewares/handleError');
