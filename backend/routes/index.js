adfdc9b1757b62f9b378d11bda6bb99a9af1e10d
Alexei Scheglov <aleshascheglov@yandex.ru>
2023-01-25 19:02:57 +0300

    fix: добавил защиту

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -37,7 +37,7 @@ routes.post(
 routes.use('/users', auth, users);
 routes.use('/cards', auth, cards);
 
-routes.all('*', (req, res, next) => {
+routes.all('*', auth, (req, res, next) => {
   next(new NotFoundError('Карточка или пользователь не найден или был запрошен несуществующий роут'));
 });
 
